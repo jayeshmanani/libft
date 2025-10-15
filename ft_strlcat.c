3c01bbaf27811bc9533dc94ab1a35ffaeff4d24e
Jayesh Manani <jmanani@3-A-4.42heilbronn.de>
2025-10-15 14:58:18 +0200

    updated memmove strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,31 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 16:06:54 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/14 16:06:54 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/15 14:57:38 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	index;
+	unsigned int	j_index;
+	int				i;
+
+	i = 0;
+	index = 0;
+	j_index = 0;
+	while (dest[index] != 0)
+		index++;
+	while (src[j_index] != 0)
+		j_index++;
+	if (index >= size)
+		return (j_index + size);
+	while ((src[i] != '\0') && ((index + i) < (size - 1)))
+	{
+		dest[index + i] = src[i];
+		i++;
+	}
+	dest[index + i] = '\0';
+	return (index + j_index);
+}
