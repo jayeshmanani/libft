6e7d50defe5233bf77a099479a8b34e4594cef79
Jayesh Manani <jmanani@3-A-4.42heilbronn.de>
2025-10-15 12:49:51 +0200

    uodated atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 15:50:22 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/14 18:36:47 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/15 12:49:30 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,35 @@ int	calc_num(char *str, int index)
 	int	num;
 
 	num = 0;
-	while (str[index] != 0)
+	while (str[index] != 0 && str[index] >= '0' && str[index] <= '9')
 	{
-		if (str[index] >= '0' && str[index] <= '9')
-		{
-			num *= 10;
-			num += str[index] - '0';
-			index++;
-		}
-		else
-			break ;
+		num *= 10;
+		num += str[index] - '0';
+		index++;
 	}
 	return (num);
 }
 
 int	ft_atoi(char *str)
 {
-	int	index;
-	int	mul;
+	int num;
+	int count_neg;
+	int index;
 
-	mul = 1;
+	num = 0;
+	count_neg = 0;
 	index = 0;
-	if (*str == '-')
+	while ((str[index] != 0) && ((str[index] < 14 && str[index] > 8)
+			|| (str[index] == 32)))
+		index++;
+	while (((str[index] == '-') || (str[index] == '+')) && (str[index] != 0))
 	{
-		mul *= -1;
-		index = 1;
+		if (str[index] == '-')
+			count_neg++;
+		index++;
 	}
-	return (mul * calc_num(str, index));
+	num = calc_num(str, index);
+	if (count_neg % 2 == 0)
+		return (num);
+	return (-num);
 }
--- libft.h
+++ libft.h
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 12:35:49 by jmanani           #+#    #+#             */
+/*   Updated: 2025/10/15 12:48:09 by jmanani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef _LIBFT_H_
 # define _LIBFT_H_
 
 int	ft_toupper(int inp);
 int	ft_tolower(int inp);
 int	ft_atoi(char *inp);
-int ft_isalpha(int inp);
+int	ft_isalpha(int inp);
 
 #endif
