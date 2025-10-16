7e0234cc69c8a2528070f5e28d34b0a73ef072ed
Jayesh Manani <jmanani@3-B-5.42heilbronn.de>
2025-10-16 18:05:42 +0200

    workung with ft strtrim 26

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 12:35:49 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/16 16:15:28 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/16 18:05:36 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ int		ft_atoi(const char *inp);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 
-// Part 2 - Additional functions (24-25)
+// Part 2 - Additional functions (24-26)
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
