8c6856b98a3a935f6e9661000673ede2e0ec7105
Jayesh Manani <jmanani@3-A-2.42heilbronn.de>
2025-10-16 13:12:11 +0200

    added calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,37 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 16:06:21 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/14 16:06:22 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/16 13:11:47 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*space;
+	int		ind;
+
+	ind = 0;
+	space = malloc(nmemb * size);
+	if (!space)
+		return (0);
+	ft_bzero(space, nmemb * size);
+	return (space);
+}
+
+// int	main(void)
+// {
+// 	int size = 8539;
+
+// 	void *d1 = ft_calloc(size, sizeof(int));
+// 	void *d2 = calloc(size, sizeof(int));
+
+//     int i = -1;
+//     while(i++ < size){
+//         printf("For D1 ft: , - %d, - %d\n", i, d1[i]);
+//         printf("For D2 ft: , - %d, - %d\n", i, d2[i]);
+//     }
+
+// 	// printf("is true? %d\n", d1 == d2);
+// }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 12:35:49 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/16 12:06:11 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/16 12:07:01 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *inp);
-// calloc
+void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 
 #endif
