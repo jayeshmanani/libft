b52bc68cf037a60f71e426c30f977b89c8ed340e
Jayesh Manani <jmanani@3-B-5.42heilbronn.de>
2025-10-17 13:56:01 +0200

    working ft substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 13:25:10 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/17 12:50:32 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/17 13:55:44 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*new_str;
 	int		i;
-	int		str_len;
+	size_t	str_len;
+	size_t	actual_len;
 
 	i = 0;
+	actual_len = len;
 	str_len = ft_strlen(s);
-	if (str_len < len)
-		new_str = (char *)malloc(sizeof(char) * (1));
-	else
-		new_str = (char *)malloc(sizeof(char) * (len + 1));
+	actual_len = str_len - start;
+	if (!*s || str_len < actual_len || start > str_len || len == 0
+		|| actual_len == 0)
+		return (ft_strdup(""));
+	if (actual_len > len)
+		actual_len = len;
+	new_str = (char *)malloc(sizeof(char) * (actual_len + 1));
 	if (!new_str)
 		return (0);
-	while ((start + i < start + len) && (start < str_len))
-	{
-		new_str[i++] = s[start + i];
-	}
-	new_str[i] = '\0';
+	if (start < str_len)
+		ft_strlcpy(new_str, &s[start], actual_len + 1);
 	return (new_str);
 }
