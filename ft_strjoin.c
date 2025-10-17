31c8d2c2d74bf17c993dc2d23d72a5848ccf96ba
Jayesh Manani <jmanani@3-B-5.42heilbronn.de>
2025-10-17 12:55:28 +0200

    working ft_strjoin t234

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 13:45:10 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/16 14:19:24 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/17 12:54:59 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	j = 0;
 	len = ft_strlen(s1) + ft_strlen(s2);
+	if(!len)
+		return ft_strdup("");
 	new_str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!new_str)
 		return (0);
-	ft_strlcpy(new_str, s1, len);
+	ft_strlcpy(new_str, s1, len + 1);
 	ft_strlcat(new_str, s2, len + 1);
 	return (new_str);
 }
