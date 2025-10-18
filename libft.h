d13ae2207a3935278022f80ba1587bfd47cecdb1
Jayesh Manani <jmanani@3-B-5.42heilbronn.de>
2025-10-18 15:32:38 +0200

    updated libft with itoa fn proto

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 12:35:49 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/17 14:54:08 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/18 14:45:52 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,11 @@ int		ft_atoi(const char *inp);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 
-// Part 2 - Additional functions (24-27)
+// Part 2 - Additional functions (24-28)
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
