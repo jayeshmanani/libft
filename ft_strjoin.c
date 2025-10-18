52e2ca449678f494a8084fe2bf03fb7a08e848d9
Jayesh Manani <jmanani@3-B-5.42heilbronn.de>
2025-10-18 14:43:38 +0200

    updated strjoin formatting

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 13:45:10 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/17 12:54:59 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/18 14:42:25 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	j = 0;
 	len = ft_strlen(s1) + ft_strlen(s2);
-	if(!len)
-		return ft_strdup("");
+	if (!len)
+		return (ft_strdup(""));
 	new_str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!new_str)
 		return (0);
