e7a0eae1bbd59fedafcfcda38df93af3eb1faed0
Jayesh Manani <jmanani@3-B-5.42heilbronn.de>
2025-10-18 15:32:11 +0200

    workding itoa

---
+++ ft_itoa.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/18 14:45:00 by jmanani           #+#    #+#             */
+/*   Updated: 2025/10/18 15:31:59 by jmanani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	count_digits(long int n)
+{
+	size_t	count;
+
+	count = 0;
+	while (n)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	get_digit(long int n, size_t multiple)
+{
+	while (multiple-- && n > 0)
+		n /= 10;
+	return ((n % 10) + '0');
+}
+
+char	*write_array(long int num, bool is_neg, size_t digits)
+{
+	size_t	i;
+	char	*arr;
+
+	i = 0;
+	arr = (char *)ft_calloc(digits + 1, sizeof(char));
+	if (!arr)
+		return (NULL);
+	if (is_neg)
+	{
+		arr[0] = '-';
+		i++;
+	}
+	while (i < digits)
+	{
+		arr[i] = get_digit(num, (digits - i - 1));
+		i++;
+	}
+	return (arr);
+}
+
+char	*ft_itoa(int n)
+{
+	size_t		digits;
+	size_t		i;
+	bool		is_neg;
+	long int	num;
+
+	is_neg = false;
+	if (n == 0)
+		return (ft_strdup("0"));
+	num = n;
+	if (n < 0)
+	{
+		is_neg = true;
+		num = -num;
+	}
+	digits = count_digits(num);
+	if (is_neg)
+		digits++;
+	return (write_array(num, is_neg, digits));
+}
+
+// int main(){
+//     printf("FT itoa return (value is : %s\n", ft_itoa(1000034)));
+// }
