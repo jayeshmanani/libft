4c658b933e2d8521a9a1d0b364c9b689a44380a3
Jayesh Manani <jmanani@3-B-5.42heilbronn.de>
2025-10-19 16:24:25 +0200

    working lstsize updated

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 12:42:32 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/19 12:47:55 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/19 16:24:08 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,9 @@
 
 int	ft_lstsize(t_list *lst)
 {
-	int	len;
-
-	len = 0;
-	while (lst != NULL)
-	{
-		len++;
+	if (lst == NULL)
+		return (0);
+	else
 		lst = lst->next;
-	}
-	return (len);
+	return (1 + ft_lstsize(lst));
 }
