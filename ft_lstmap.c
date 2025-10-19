ce10b373491eb25d4fc229d1e3d755fbd5b6595c
Jayesh Manani <jmanani@3-C-6.42heilbronn.de>
2025-10-19 18:56:32 +0200

    updated with del content in lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 17:39:49 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/19 18:13:39 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/19 18:55:20 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*res;
 	t_list	*temp;
+	void	*content;
 
 	if (!lst && !f && !del)
 		return (NULL);
 	res = NULL;
 	while (lst != NULL)
 	{
-		temp = ft_lstnew((void *)f(lst->content));
+		content = f(lst->content);
+		temp = ft_lstnew(content);
 		if (!temp)
 		{
+			del(content);
 			ft_lstclear(&res, del);
 			return (NULL);
 		}
