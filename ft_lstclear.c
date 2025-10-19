50721f89c1e10c215469cdce7f1127ebfeeb9a79
Jayesh Manani <jmanani@3-B-5.42heilbronn.de>
2025-10-19 17:34:12 +0200

    working files lstclear lstdelone

---
+++ ft_lstclear.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 17:09:26 by jmanani           #+#    #+#             */
+/*   Updated: 2025/10/19 17:32:04 by jmanani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*lst_new;
+	t_list	*lst_cur;
+
+	if (lst && del)
+	{
+		lst_cur = *lst;
+		while (lst_cur != NULL)
+		{
+			lst_new = lst_cur->next;
+			del(lst_cur->content);
+			free(lst_cur);
+			lst_cur = lst_new;
+		}
+		*lst = NULL;
+	}
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 12:35:49 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/19 16:24:36 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/19 17:09:59 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,17 +59,20 @@ void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 
-// Bonus Part (35-39)
+// Structure Definition for the Bonus Part
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
 
+// Bonus Part (35-41)
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif
