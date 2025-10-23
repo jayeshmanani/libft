a290726e69df3ed881a38e9e23f6dc84d49270bf
Jayesh Manani <jmanani@3-A-15.42heilbronn.de>
2025-10-23 16:19:23 +0200

    upadte: memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 16:06:43 by jmanani           #+#    #+#             */
-/*   Updated: 2025/10/15 15:59:37 by jmanani          ###   ########.fr       */
+/*   Updated: 2025/10/23 16:19:12 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,17 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	char	*temp_dest;
 	char	*temp_src;
+	size_t	i;
 
+	i = 0;
 	if (dest == 0 && src == 0)
-		return (0);
+		return (NULL);
 	temp_dest = (char *)dest;
 	temp_src = (char *)src;
-	while (n--)
+	while (i < n)
 	{
 		*temp_dest++ = *temp_src++;
+		i++;
 	}
 	return (dest);
 }
