3d3b639f88e76b12eb7716901e5471f07dbe0628
Jayesh Manani <jsmanani@gmail.com>
2025-10-26 18:04:38 +0100

    upd: strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -17,6 +17,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		len;
 	char	*new_str;
 
+	if (!s1 || !s2)
+		return (NULL);
 	len = ft_strlen(s1) + ft_strlen(s2);
 	if (!len)
 		return (ft_strdup(""));
